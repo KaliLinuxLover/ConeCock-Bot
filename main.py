9d56a772862993edf62f23d6c3b89bb6d3683c0c
Alexander Miller <54007296+Charmander1011@users.noreply.github.com>
2021-09-17 21:03:10 +0000

    made the bot mean

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  if message.content.startswith('Hello'):
+    await message.channel.send('your are not loved.')
 
 
 keep_alive()
