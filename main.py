ff421498c59c9763fa78b0cf28d51d8e3739691c
Alexander Miller <54007296+Charmander1011@users.noreply.github.com>
2021-09-19 06:44:38 +0000

    ellas word list added

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import os
 import requests
 import json
 import flask
+import random
 from keep_alive import keep_alive
 from discord.utils import get
 
 client = discord.Client()
+
+ella_words = ["joke", "joking", "jk", "fat second", "let us in the penthouse", "stfu", "quirky", ]
+
+ella_responses = [
+  "Kys",
+  "no"
+]
+
 
 @client.event
 async def on_ready():
@@ -26,7 +35,16 @@ async def on_message(message):
     await message.channel.send('your are not loved.')
 
   if message.content.startswith('thank you'):
-    await message.channel.send('Welcome. BTW Your are adopted.')
+    await message.channel.send('Welcome. BTW your are adopted.')
+
+  if message.content.startswith('github-repo'):
+    await message.channel.send('My repo is https://github.com/Charmander1011/ConeCock-Bot now go kys.')
+  
+  if any(word in message.content for word in ella_words):
+    await message.channel.send(random.choice(ella_responses))
+  
+  if message.content.startswith('kys'):
+    await message.channel.send('no you')
 
 
 keep_alive()
